eb80789051ea975a555c1152b89f773e528c204b
Brittany Kerr <kerrbrittany9@gmail.com>
2017-08-04 10:23:31 -0700

    rename js file names to match illness object

--- js/doctor.js
+++
@@ -1,5 +0,0 @@
-function Illness () {
-
-}
-
-exports.illnessModule = Illness;
---
+++ js/illness-interface.js
@@ -0,0 +1,14 @@
+var Illness = require('./../js/illness.js').illnessModule;
+
+// var displayDoctors = function(medicalIssue, doctorList) {
+//   $('#showDoctors').append("Doctors who treat the following: " + medicalIssue + "are listed below:" + "<ul><li>" + doctorList + "</li></ul>");
+// };
+
+$(document).ready(function() {
+  $('#symptom-form').click(function() {
+    var medicalIssue = $('#medicalIssue').val();
+    $('#medicalIssue').val("");
+    illness.getDoctors(medicalIssue);
+
+  });
+});
