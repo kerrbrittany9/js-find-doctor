c8d6ee79946bbea4411f5633f4edbf1e2ad7568d
Brittany Kerr <kerrbrittany9@gmail.com>
2017-08-04 15:58:24 -0700

    final spacing issues

--- js/illness.js
+++ js/illness.js
@@ -7,7 +7,6 @@ Illness.prototype.getDoctors = function(medicalIssue, displayDoctors) {
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue +'&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
       displayDoctors(medicalIssue, result.data);
-      console.log(result.data);
     })
    .fail(function(error) {
       $('#showDoctors').text(error);
